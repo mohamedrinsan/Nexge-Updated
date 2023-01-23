eab37d405b0c2a0fe4325279a0693e5068047af6
mohamedrinsan <mohamedrinsan007@gmail.com>
2023-01-23 22:10:36 +0530

    nexgeupdate18

--- scripts.js
+++ scripts.js
@@ -252,38 +252,6 @@
   document.addEventListener("mouseup", drag_Stop);
   caro_usel.addEventListener("touchend", drag_Stop);
   
-  new Swiper('.recent-photos-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      },
-      1200: {
-        slidesPerView: 5,
-        spaceBetween: 20
-      }
-    }
-  });
 
   new Swipers('.recent-photos-slider', {
     speed: 400,
